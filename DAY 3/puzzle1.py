d50d049f2c414115f5c39117ade6d0423314ab10
federicobrancasi <federicobrancasi@gmail.com>
2022-12-03 15:51:21 +0100

    DAY 3: Rucksacks

---
+++ DAY 3/puzzle1.py
@@ -0,0 +1,41 @@
+# function get_priority for getting the priority of the given character
+def get_priority(character):
+    # if the character is a lower case letter then return its ascii value - 96
+    if character.islower():
+        return ord(character) - 96
+    # if the character is an upper case letter then return its ascii value - 38
+    elif character.isupper():
+        return ord(character) - 38
+
+
+# initialize the sum of the priorities
+priority_sum = 0
+
+# open the file
+with open('input.txt') as f:
+    # read the lines
+    lines = f.readlines()
+
+    # for each line
+    for line in lines:
+        # get the length of the line
+        length = len(line)
+        # get the first half of the line
+        first_half = line[:length // 2]
+        # get the second half of the line
+        second_half = line[length // 2:]
+
+        # for each character in the first half
+        for char in first_half:
+            # if the character is in the second half
+            if char in second_half:
+                # sum the priorities
+                priority_sum += get_priority(char)
+                # break the loop
+                break
+
+# print the sum
+print(priority_sum)
+
+# close the file
+f.close()
---
+++ DAY 3/puzzle2.py
@@ -0,0 +1,34 @@
+# function get_priority for getting the priority of the given character
+def get_priority(character):
+    # if the character is a lower case letter then return its ascii value - 96
+    if character.islower():
+        return ord(character) - 96
+    # if the character is an upper case letter then return its ascii value - 38
+    elif character.isupper():
+        return ord(character) - 38
+
+
+# initialize the sum of the priorities
+priority_sum = 0
+
+# open the file
+with open('input.txt') as f:
+    # read the lines
+    lines = f.readlines()
+
+    # read 3 lines at a time (they identify a group)
+    for line1, line2, line3 in zip(lines[::3], lines[1::3], lines[2::3]):
+        # find the common character that appears in all 3 lines
+        common_char = set(line1).intersection(line2, line3)
+        # and eliminate the elements that are not letters (i.e. the newline character)
+        common_char = [char for char in common_char if char.isalpha()]
+        # if there is a common character (must be only one)
+        if common_char:
+            # sum the priorities
+            priority_sum += get_priority(common_char[0])
+
+# print the sum
+print(priority_sum)
+
+# close the file
+f.close()
