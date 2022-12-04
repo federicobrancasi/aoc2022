972df5fd1fc929bc221f07692ac4e3a61da4b424
federicobrancasi <federicobrancasi@gmail.com>
2022-12-04 15:15:41 +0100

    DAY 4: Camp Cleanup

---
+++ DAY 4/puzzle1.py
@@ -0,0 +1,36 @@
+def create_array(string):
+    # split the string by the - character
+    split_string = string.split("-")
+    # create an array with the numbers in the range
+    array = range(int(split_string[0]), int(split_string[1]) + 1)
+    # return the array
+    return array
+
+
+# initialize the sum of the priorities
+pair_sum = 0
+
+# open the file
+with open('input.txt') as f:
+    # read the lines
+    lines = f.readlines()
+
+    # iterate over the lines
+    for line in lines:
+        # remove "\n" from the line
+        line = line.replace("\n", "")
+
+        # check if the first array is fully contained in the second array
+        if all(x in create_array(line.split(",")[1]) for x in create_array(line.split(",")[0])):
+            # add one to the pair sum
+            pair_sum += 1
+        # or check if the second array is fully contained in the first array
+        elif all(x in create_array(line.split(",")[0]) for x in create_array(line.split(",")[1])):
+            # add one to the pair sum
+            pair_sum += 1
+
+# print the sum
+print(pair_sum)
+
+# close the file
+f.close()
